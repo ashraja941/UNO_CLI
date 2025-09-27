9683b3f6bd6d3b4548f87bda2b78ee8cca042231
ash <ashraja941@gmail.com>
2025-09-27 00:20:22 -0500

    added card creation logic

---
+++ src/card.zig
@@ -0,0 +1,70 @@
+const std = @import("std");
+const expect = std.testing.expect;
+const expectError = std.testing.expectError;
+
+pub const InitializationError = error{
+    InvalidNumber,
+    InvalidColor,
+};
+
+const CardColor = enum { RED, BLUE, GREEN, YELLOW, WILDCOLOR };
+
+const CardType = union(enum) {
+    NUMBER: u4,
+    SKIP: void,
+    REVERSE: void,
+    DRAW2: void,
+    WILD: void,
+    WILD4: void,
+};
+
+const Card = struct {
+    color: CardColor,
+    value: CardType,
+
+    pub fn init(color: CardColor, value: CardType) InitializationError!Card {
+        switch (value) {
+            .NUMBER => |n| {
+                if (n > 12) return InitializationError.InvalidNumber;
+                if (color == .WILDCOLOR) return InitializationError.InvalidColor;
+            },
+            .SKIP, .REVERSE, .DRAW2 => {
+                if (color == .WILDCOLOR) return InitializationError.InvalidColor;
+            },
+            .WILD, .WILD4 => {
+                if (color != .WILDCOLOR) return InitializationError.InvalidColor;
+            }
+        }
+
+        return Card{
+            .color = color,
+            .value = value,
+        };
+    }
+};
+
+test "Tagged Union type check" {
+    const ct = CardType.SKIP;
+    try expect(@as(CardType, ct) == CardType.SKIP);
+
+    const cn = CardType{ .NUMBER = 1 };
+    try expect(@as(CardType, cn) == CardType.NUMBER);
+
+    switch (cn) {
+        .NUMBER => |value| try expect(value == 1),
+        else => unreachable,
+    }
+}
+
+test "Create Card" {
+    const card = try Card.init(.RED, .SKIP);
+    try expect(card.color == CardColor.RED);
+    try expect(card.value == CardType.SKIP);
+}
+
+test "create faulty card" {
+    try expectError(InitializationError.InvalidNumber, Card.init(.BLUE, .{ .NUMBER = 13 }));
+    try expectError(InitializationError.InvalidColor, Card.init(.WILDCOLOR, .{ .NUMBER = 10 }));
+    try expectError(InitializationError.InvalidColor, Card.init(.WILDCOLOR, .SKIP));
+    try expectError(InitializationError.InvalidColor, Card.init(.BLUE, .WILD));
+}
